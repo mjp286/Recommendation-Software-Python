c51af4db3ea18bc776fc7756a504fa99aadc26e9
mjp286 <mattprogar@yahoo.com>
2023-08-22 14:18:13 -0400

    simplified code
    removed code that did not add anything to the results

--- Recomendation.py
+++ Recomendation.py
@@ -1,5 +1,5 @@
-from Data import root, genres, songs
-from BFS import getting_songs
+from Data import genres, songs
+
 
 
 def introduction():
@@ -22,58 +22,20 @@ def choices():
         else: 
             break
     
-    song = input(f"Would you like see songs\n Y'\'N: ")
+    song = input(f"Would you like see songs? Y'\'N: ")
     if song.lower() == 'y':
             format_list = []
             for item in songs[genre]:
                 format_list.append(item.capitalize().replace("_", " "))
-            print(format_list)
-            choice = choice.lower().replace(" ", ("_"))
-            if choice not in songs[genre]:
-                print("Not on the list!")
-            
+
+            if not format_list:
+                print("No songs available for this genre.")
+            else:
+                for song in format_list:
+                 print(song)
 
     return genre, choice
 
-def name_swap(string):
-    string_split = string.split()
-    if len(string_split) == 0:
-        return ""
-    surrname = string_split[0].strip().strip(',')
-    name = "---"
-    for i in string_split[1:]:
-        name += i.strip() + ' '
-    return name + surrname.strip()
-
-def recomedations():
-    (genre, sub_genre) = choices()
-    list_of_nodes = getting_songs(root, genre, sub_genre)
-    book_names = "\n"
-    for node in list_of_nodes:
-        if ',' in node.name and ('The' in node.name or 'A' in node.name):
-            string_to_split = node.name.split()
-            formated_string = ""
-            prefix = ""
-            suffix = ""
-            for string in string_to_split:
-                string = string.strip(',')
-                if string.strip() == 'A' or string.strip() == 'The':
-                    prefix += string.strip()
-                    #print(prefix)
-                else:
-                    suffix += " " + string.strip()
-                    #print(suffix)
-            
-            formated_string = prefix + suffix
-            book_names += "Title: " + formated_string + ' | Author: ' + name_swap(node.author)
-        else:
-            book_names += "Title: " + node.name + ' | Author: ' + name_swap(node.author)
-    
-        if sub_genre is None or sub_genre == '':
-            book_names += ' | Sub-Genre:' + node.sub_genre.capitalize().replace('_', ' ') + '\n'
-        else:
-            book_names += '\n'
-    print(book_names)
 
 def repeat():
     repeat_ans = input("\n Do you want to get recomendations for another genre ?  | Y'\'N: ")

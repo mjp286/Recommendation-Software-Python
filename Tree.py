32da494c44ba6ed7680d0c2402e9cb07265d5756
mjp286 <mattprogar@yahoo.com>
2023-08-22 15:54:00 -0400

    deleted unnecessary code
    final commit

--- Tree.py
+++ Tree.py
@@ -18,21 +18,5 @@ class TreeNode:
             current_node = nodes_to_visit.pop()
             print(current_node.name)
             nodes_to_visit += current_node.children
-# Tree Visualization helper ! 
-    def __str__(self):
-        stack = deque()
-        stack.append([self, 0])
-        level_str = "\n"
-        while len(stack) > 0:
-            node, level = stack.pop()
-      
-            if level > 0:
-                level_str += "| "*(level-1)+ "|-"
-            level_str += str(node.name)
-            level_str += "\n"
-            level+=1
-            for child in reversed(node.children):
-                stack.append([child, level])
 
-        return level_str
             
